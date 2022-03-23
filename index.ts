002ae23d09cc1d499c04072df5d58e4a2cb6b50a
Anik Mohammad <anik7303@gmail.com>
2022-03-24 03:37:17 +0600

    type annotations with variables

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prefer-const */
+
 let apples: number = 5;
 let speed: string = "fast";
 let hasName: boolean = true;
