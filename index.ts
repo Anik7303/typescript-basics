8a512bfc9cf77d9095292e1792eff389674724f4
Anik Mohammad <anik7303@gmail.com>
2022-03-24 05:24:45 +0600

    destructuring with type annotations

--- index.ts
+++ index.ts
@@ -25,3 +25,22 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+// destructuring annotations
+const todaysWeather: { date: Date; weather: string } = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
