d29f13d905b3c0fe66169c429dd2162fe0f85196
Anik Mohammad <anik7303@gmail.com>
2022-03-24 03:25:42 +0600

    type annotations with variables

--- index.ts
+++ index.ts
@@ -1,27 +1,9 @@
-import axios from "axios";
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
 
-const url = "https://jsonplaceholder.typicode.com/todos/1";
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
 
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-axios.get(url).then((response) => {
-  const data = response.data as Todo;
-
-  const id: number = data.id;
-  const title: string = data.title;
-  const finished: boolean = data.completed;
-
-  logTodo(id, title, finished);
-});
-
-const logTodo = (id: number, title: string, completed: boolean) => {
-  console.log(`
-  The Todo with ID: ${id}
-  Has a title of: ${title}
-  Is it finished: ${completed}
-  `);
-};
+// buildt in objects
+let now: Date = new Date();
