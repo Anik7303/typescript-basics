a79a740919f07b6be3815bbfd26e6592a3a771b7
Anik Mohammad <anik7303@gmail.com>
2022-03-24 05:18:54 +0600

    void and never annotations

--- index.ts
+++ index.ts
@@ -16,3 +16,12 @@ function multiply(a: number, b: number): number {
 const divide = function (a: number, b: number): number {
   return a / b;
 };
+
+// void annotation type
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
