c22aadf28e74aaf23d8eadc9a3a57b44d4ad6f43
Anik Mohammad <anik7303@gmail.com>
2022-03-26 01:09:59 +0600

    tuples in typescript

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-"use strict";
-exports.__esModule = true;
-var axios_1 = require("axios");
-var url = "https://jsonplaceholder.typicode.com/todos/1";
-axios_1["default"].get(url).then(function (response) {
-    var data = response.data;
-    var id = data.id;
-    var title = data.title;
-    var finished = data.completed;
-    logTodo(id, title, finished);
-});
-var logTodo = function (id, title, completed) {
-    console.log("\n  The Todo with ID: ".concat(id, "\n  Has a title of: ").concat(title, "\n  Is it finished: ").concat(completed, "\n  "));
+var drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
 };
+var pepsi = ['brown', true, 40];
--- index.ts
+++ index.ts
@@ -1,29 +1,7 @@
-const carMakers = ['ford', 'toyota', 'chevy']
-const dates = [new Date(), new Date()]
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+}
 
-// const carsByMake: string[][] = [
-//   ['f150'],
-//   ['corolla'],
-//   ['camaro']
-// ]
-const carsByMake: string[][] = []
-
-
-// Help with inference when extracting values
-const car = carMakers[0]
-const myCAr = carMakers.pop();
-
-// Prevent incompatible values
-// carMakers.push(100)
-
-// Help with 'map'
-carMakers.map((car: string):string => {
-  return car.toUpperCase();
-})
-
-// arrays with multiple types
-// const importantDates: (string | Date)[] = ['10/12/2021', new Date(), '04/04/2008']
-const importantDates = ['10/12/2021', new Date(), '04/04/2008']
-importantDates.push(new Date())
-importantDates.push('26-03-2022')
-// importantDates.push(100) // shows an error
+const pepsi: [string, boolean, number] = ['brown', true, 40]
