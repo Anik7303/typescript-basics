f89482b1e3f7b98ab06227516b454dff47c40fdf
Anik Mohammad <anik7303@gmail.com>
2022-03-27 05:53:21 +0600

    functions in interfaces

--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
 // Interfaces
 
 interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+  summary(): string;
 }
 
 const oldChivic = {
-  name: 'chivic',
+  name: "chivic",
   year: 2000,
-  broken: true
-}
+  broken: true,
+  summary() {
+    return `Name: ${this.name}`;
+  },
+};
 
 // const printVehicle = (vehicle: {name: string; year: number; broken: boolean}): void => {
 const printVehicle = (vehicle: Vehicle): void => {
-console.log(`Name: ${vehicle.name}`)
-  console.log(`Year: ${vehicle.year}`)
-  console.log(`Broken? ${vehicle.broken}`)
-}
+  console.log(vehicle.summary);
+};
 
-printVehicle(oldChivic)
+printVehicle(oldChivic);
