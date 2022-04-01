df00efd7bab52b29903f583ff03d5d3b75ae7d4a
Anik Mohammad <anik7303@gmail.com>
2022-04-01 16:55:27 +0600

    adding a google map

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
+const element = document.getElementById("map")!;
 
-console.log({ user, company });
+new google.maps.Map(element, {
+  zoom: 3,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
