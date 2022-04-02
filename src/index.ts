d85f4f82ac375c44ebe3dc13114c4e17e1ead5e4
Anik Mohammad <anik7303@gmail.com>
2022-04-03 02:50:45 +0600

    implementing characters collection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([7, 2, 0, -3, 10, -5]);
 const numbersSorter = new Sorter(numbersCollection);
 numbersSorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xaaaa");
+const charactersSorter = new Sorter(charactersCollection);
+charactersSorter.sort();
+console.log(charactersCollection.data);
