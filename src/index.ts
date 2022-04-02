e770b2cbf84915aa4b48316db98e048d34c3959f
Anik Mohammad <anik7303@gmail.com>
2022-04-03 02:12:20 +0600

    sorting implemented for array of numbers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Welcome to Typescript Development!");
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length - 1; i++) {
+      let lim = length - i - 1;
+      for (let j = 0; j < lim; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          let temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const arr = new Sorter([7, 2, 0, -3]);
+arr.sort();
+console.log(arr.collection);
