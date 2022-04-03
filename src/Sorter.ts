c60ddea9e9f72e65aab0dac13032ae4f0874f3c5
Anik Mohammad <anik7303@gmail.com>
2022-04-04 01:55:26 +0600

    using Sorter as abstract class

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,20 +1,22 @@
 export interface Sortable {
+  length: number;
   compare(leftIndex: number, rightIndex: number): boolean;
   swap(leftIndex: number, rightIndex: number): void;
-  length: number;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  abstract length: number;
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length - 1; i++) {
       let lim = length - i - 1;
       for (let j = 0; j < lim; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([7, 2, 0, -3, 10, -5]);
-const numbersSorter = new Sorter(numbersCollection);
-numbersSorter.sort();
+const numbersCollection = new NumbersCollection([-7, 2, 0, -3, 10]);
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaaaa");
-const charactersSorter = new Sorter(charactersCollection);
-charactersSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
 
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+linkedList.add(-7);
+linkedList.add(2);
+linkedList.add(0);
+linkedList.add(-3);
+linkedList.add(10);
+
+linkedList.sort();
 linkedList.print();
