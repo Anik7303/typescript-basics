fe6e9f37e308ac90df4207276bfd49b67f23cd64
Anik Mohammad <anik7303@gmail.com>
2022-04-05 01:31:20 +0600

    generics with functions

--- index.ts
+++ index.ts
@@ -9,3 +9,13 @@ class ArrayOfAnything<T> {
 // const arr = new ArrayOfAnything<string>(["a", "b", "c", "d"]);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const arr = new ArrayOfAnything(["a", "b", "c", "d"]); // This is equivalant to the previous line because of type inference in typescript
+
+// Generics with Functions
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+// printAnything<number>([1, 2, 3, 4])
+printAnything([1, 2, 3, 4]);
