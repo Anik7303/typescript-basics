5e47be3565de0ab8f05b5bad05809a83861eb79b
Anik Mohammad <anik7303@gmail.com>
2022-04-08 15:50:01 +0600

    setup User property set & update and User eventing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("test done!");
+import { User } from "./models/User";
+
+const user = new User({ name: "anik", age: 28 });
+
+user.set({ age: 30 });
+
+console.log(user.get("name"), user.get("age"));
