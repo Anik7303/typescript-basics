57b5e186ad286f04ca747834b8fd5d2c24ca677c
Anik Mohammad <anik7303@gmail.com>
2022-05-16 22:05:40 +0600

    more on decorators

--- index.ts
+++ index.ts
@@ -1,17 +1,37 @@
+@classDecorator
 class Boat {
   @testDecorator
   private color = "red";
 
+  @testDecorator
   get formattedColor(): string {
     return `This boat is colored ${this.color}`;
   }
 
   @logError("Oops, the boat sank in ocean")
-  pilot(): void {
-    throw new Error();
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === "fast") {
+      console.log("swish");
+    } else {
+      console.log("nothing");
+    }
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function classDecorator(constructor: typeof Boat): void {
+  console.log(constructor);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function parameterDecorator(target: any, key: string, index: number): void {
+  console.log({ target, key, index });
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function testDecorator(target: any, key: string): void {
   console.log({ target, key });
 }
