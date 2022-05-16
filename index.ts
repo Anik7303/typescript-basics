631e685436a98ada1d5c46285040c2d5b86466a2
Anik Mohammad <anik7303@gmail.com>
2022-05-16 21:54:25 +0600

    using decorator factory

--- index.ts
+++ index.ts
@@ -5,23 +5,30 @@ class Boat {
     return `This boat is colored ${this.color}`;
   }
 
-  @logError
+  @logError("Oops, the boat sunk in ocean")
   pilot(): void {
     throw new Error();
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  console.log({ target, key, desc });
-  const method = desc.value;
+// eslint-disable-next-line @typescript-eslint/ban-types
+function logError(message: string): Function {
+  return function (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    target: any,
+    key: string,
+    desc: PropertyDescriptor
+  ): void {
+    console.log({ target, key, desc });
+    const method = desc.value;
 
-  desc.value = function (): void {
-    try {
-      method();
-    } catch (e) {
-      console.error(e);
-    }
+    desc.value = function (): void {
+      try {
+        method();
+      } catch (e) {
+        console.error(message);
+      }
+    };
   };
 }
 
