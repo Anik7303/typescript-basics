9cf291091bc994aed5ff0e7df2796b338d974291
Anik Mohammad <anik7303@gmail.com>
2022-05-16 21:51:39 +0600

    using property descriptor

--- index.ts
+++ index.ts
@@ -7,11 +7,22 @@ class Boat {
 
   @logError
   pilot(): void {
-    console.log("swish");
+    throw new Error();
   }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function logError(target: any, key: string): void {
-  console.log({ target, key });
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  console.log({ target, key, desc });
+  const method = desc.value;
+
+  desc.value = function (): void {
+    try {
+      method();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 }
+
+new Boat().pilot();
