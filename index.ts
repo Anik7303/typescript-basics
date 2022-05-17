ee02bbf983eb534fa59791ef6fc1ba9483352ce2
Anik Mohammad <anik7303@gmail.com>
2022-05-18 00:37:20 +0600

    intro to metadata

--- index.ts
+++ index.ts
@@ -1,58 +1,19 @@
-@classDecorator
-class Boat {
-  @testDecorator
-  private color = "red";
+import "reflect-metadata";
 
-  @testDecorator
-  get formattedColor(): string {
-    return `This boat is colored ${this.color}`;
-  }
+class Plane {
+  color = "red";
 
-  @logError("Oops, the boat sank in ocean")
-  pilot(
-    @parameterDecorator speed: string,
-    @parameterDecorator generateWake: boolean
-  ): void {
-    if (speed === "fast") {
-      console.log("swish");
-    } else {
-      console.log("nothing");
-    }
+  fly(): void {
+    console.log("vrrrrr");
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function classDecorator(constructor: typeof Boat): void {
-  console.log(constructor);
-}
+// assign metadata to a class
+// Reflect.defineMetadata("secret", "secret info", Plane);
+// const secret = Reflect.getMetadata("secret", Plane);
+// console.log({ secret });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function parameterDecorator(target: any, key: string, index: number): void {
-  console.log({ target, key, index });
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function testDecorator(target: any, key: string): void {
-  console.log({ target, key });
-}
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-function logError(message: string): Function {
-  return function (
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    target: any,
-    key: string,
-    desc: PropertyDescriptor
-  ): void {
-    console.log({ target, key, desc });
-    const method = desc.value;
-
-    desc.value = function (): void {
-      try {
-        method();
-      } catch (e) {
-        console.error(message);
-      }
-    };
-  };
-}
+// assign metadata to a property of class
+Reflect.defineMetadata("secret", "secret info", Plane, "color");
+const secret = Reflect.getMetadata("secret", Plane, "color");
+console.log({ secret });
